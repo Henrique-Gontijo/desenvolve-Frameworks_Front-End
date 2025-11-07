2b30478dca8ea62bb6e9ce9052acd33a5725c44e
Henrique-Gontijo <henrique.gontijo@pdbomdespacho.com.br>
2025-11-07 14:32:33 -0300

    Aula 21 - Estruturando um site

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+//? Código Popovers
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+//? Código Tooltips
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
