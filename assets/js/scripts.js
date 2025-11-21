d1539131b8e9e1dce5ee24cd07ce301ffce1c685
Henrique-Gontijo <henrique.gontijo@pdbomdespacho.com.br>
2025-11-21 16:20:28 -0300

    Aula 35 - Publicando seu site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,10 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//? Modal
+const modalPortfolio = document.getElementById('modalPortfolio');
+
+modalPortfolio.addEventListener('show.bs.modal', function (event) {
+    document.getElementById('modalImage').src = event.relatedTarget.getAttribute('data-img');
+});
